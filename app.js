47d2adea9bf53bc39f4c5532d56b9c43f8863ea4
Silva liao <silvaliao@Silvas-MacBook-Pro.local>
2023-11-20 20:49:22 -0800

    my second try

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import "dotenv/config";
+import session from "express-session";
 
 
 
