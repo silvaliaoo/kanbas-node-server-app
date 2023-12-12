7d4abb20755d4032cd1979a7af52cda244e73e0d
Silva liao <liao.suj@northeastern.edugit config --global user.name silvaliaoo>
2023-12-11 16:26:22 -0800

    a6

--- app.js
+++ app.js
@@ -4,19 +4,57 @@ import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
 import "dotenv/config";
 import session from "express-session";
 
+import UserRoutes from "./users/routes.js";
 
+
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas';
+
+mongoose.connect(CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+ 
+    })
+   );
+
+   const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+  
 app.use(express.json());
+const port = process.env.PORT || 4000;
+
+UserRoutes(app);
 ModuleRoutes(app);
 
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
 
-app.listen(process.env.PORT || 4000);
+
+
+
+app.listen(4000);
 
